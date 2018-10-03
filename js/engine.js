68c8170089cd64bfe0e3794cca8b9d033b6a7db0
Delaney Hanson <delaney.hanson@techtonic.com>
2018-10-03 15:00:34 -0600

    adds several functions of the library and commented out books

--- js/engine.js
+++ js/engine.js
@@ -3,10 +3,70 @@ function Library(){
 };
 
 Library.prototype.addBook = function (book) {
+  for(var i=0; i <this.bookShelf.length; i++) {
+    if(this.bookShelf[i].title === book.title) {
+      return false;
+    };
+  };
 
+  this.bookShelf.push(book);
+  return true;
 };
 
 Library.prototype.removeBookByTitle = function (title) {
+  for(var i=0; i <this.bookShelf.length; i++) {
+    if(this.bookShelf[i].title === title) {
+      this.bookShelf.splice(i,1);
+      return true;
+    };
+  };
+  return false;
+};
+
+Library.prototype.removeBookByAuthor = function (author) {
+  for(var i=0; i <this.bookShelf.length; i++) {
+    if(this.bookShelf[i].author === author) {
+      this.bookShelf.splice(i,1);
+      return true;
+    };
+  };
+  return false;
+};
+
+Library.prototype.getRandomBook = function () {
+  var randomBook = this.bookShelf[Math.floor(Math.random()*this.bookShelf.length)];
+    if(this.bookShelf.length > 0) {
+      return randomBook;
+    }else {
+      return null;
+  };
+};
+
+Library.prototype.getBookByTitle = function (title) {
+  var booksWithTitle = new Array();
+  for(var i=0; i <this.bookShelf.length; i++) {
+    if(this.bookShelf[i].title.includes(title) || this.bookShelf[i].title === title) {
+      var theRightBook = this.bookShelf[i].title;
+      booksWithTitle.push(theRightBook);
+    };
+
+  };
+  return booksWithTitle;
+};
+
+Library.prototype.getBookByAuthor = function (author) {
+  var booksWithAuthor = new Array();
+  for(var i=0; i <this.bookShelf.length; i++) {
+    if(this.bookShelf[i].author.includes(author) || this.bookShelf[i].author === author) {
+      var theRightAuthor = this.bookShelf[i].title;
+      booksWithAuthor.push(theRightAuthor);
+    };
+
+  };
+  return booksWithAuthor;
+};
+
+Library.prototype.addBooks = function (books) {
 
 };
 
