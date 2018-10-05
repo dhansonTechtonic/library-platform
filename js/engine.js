5a61613a69dc574cd28fee7b41f518292d9a56af
Delaney Hanson <delaney.hanson@techtonic.com>
2018-10-05 13:31:45 -0600

    edits format of comments

--- js/engine.js
+++ js/engine.js
@@ -12,11 +12,9 @@
 
 /*
 addBook: I want to loop through the bookShelf and see if there are already
-books with that title.
-If there are then return false and don't add a book
-If there aren't then add the book to the bookShelf array
-Then send the updates to the localStorage and return true to make sure the
-book was added
+books with that title. If there are then return false and don't add a book
+If there aren't then add the book to the bookShelf array. Then send the updates
+to the localStorage and return true to make sure the book was added.
 */
 Library.prototype.addBook = function (book) {
   for(var i=0; i <this.bookShelf.length; i++) {
@@ -31,10 +29,9 @@ Library.prototype.addBook = function (book) {
 
 /*
 removeBookByTitle: I want to loop through the bookShelf and see if there is a
-book that matches the title that was inputted.
-If there is then I want to remove that book from my array, update localStorage,
-and return true
-If there is not then I want to return false
+book that matches the title that was inputted. If there is then I want to remove
+that book from my array, update localStorage, and return true. If there is not
+then I want to return false.
 */
 Library.prototype.removeBookByTitle = function (title) {
   for(var i=0; i <this.bookShelf.length; i++) {
@@ -49,7 +46,7 @@ Library.prototype.removeBookByTitle = function (title) {
 
 /*
 removeBookByAuthor: same as remove book by title but I have to make sure the
-loop starts in the right place and doesn't skip over any books
+loop starts in the right place and doesn't skip over any books.
 */
 Library.prototype.removeBookByAuthor = function (author) {
   var booksRemoved = 0;
@@ -67,9 +64,11 @@ Library.prototype.removeBookByAuthor = function (author) {
   return false;
 };
 
-//getRandomBook: I want to produce a random title and if there are books in the
-//array then I want to return that random book title.
-//If there are not then I want to return null
+/*
+getRandomBook: I want to produce a random title and if there are books in the
+array then I want to return that random book title. If there are not then I want
+to return null.
+*/
 Library.prototype.getRandomBook = function () {
   var randomBook = this.bookShelf[Math.floor(Math.random()*this.bookShelf.length)];
     if(this.bookShelf.length > 0) {
@@ -82,8 +81,8 @@ Library.prototype.getRandomBook = function () {
 /*
 getBookByTitle: I want to creat an array that will hold the book results
 then I want to loop through the bookShelf and see if there are titles that
-have all or some of what was inputted
-If there is then I want to add those books to the array and return the array
+have all or some of what was inputted. If there is then I want to add those
+books to the array and return the array.
 */
 Library.prototype.getBookByTitle = function (title) {
   var booksWithTitle = [];
@@ -109,11 +108,10 @@ Library.prototype.getBookByAuthor = function (author) {
 };
 
 /*
-addBooks: I want to start a count of how many books are added to my bookShelf
-I want to add books using my addBook function
-I want to loop through the array of books that are added and count one for
-each book added
-Then I want to save to localStorage and return how many books were added
+addBooks: I want to start a count of how many books are added to my bookShelf. I
+want to add books using my addBook function. I want to loop through the array of
+books that are added and count one for each book added. Then I want to save to
+localStorage and return how many books were added.
 */
 Library.prototype.addBooks = function (books) {
   var numberOfAdds = 0
@@ -127,13 +125,11 @@ Library.prototype.addBooks = function (books) {
 };
 
 /*
-getAuthors: I want to create an array that displays all the authors
-If there are no books in the bookShelf return foundAuthors(nothing)
-If there are books then add the first author to the array to initiate the loop
-Then I want to loop through my bookShelf and through my foundAuthors and see
-if they have any names that aren't in foundAuthors then add them
-If they are there then skip over them
-Then return the authors
+getAuthors: I want to create an array that displays all the authors. If there are no
+books in the bookShelf return foundAuthors(nothing). If there are books then add the
+first author to the array to initiate the loop. Then I want to loop through my bookShelf
+and through my foundAuthors and see if they have any names that aren't in foundAuthors
+then add them. If they are there then skip over them. Then return the authors.
 */
 Library.prototype.getAuthors = function () {
   var foundAuthors = [];
@@ -164,13 +160,10 @@ Library.prototype.getRandomAuthorName = function () {
 
 /*
 theBigSearch: I want to take in the user's input and separate it into separate
-strings
-Then I want to create an array for my results
-Then I want to loop through the strings and use getBookByTitle and
-getBookByAuthor to push books that match those strings
-Then I want to create a new array that merges results and filters through and
-get rid of duplicates
-Then return the merged results
+strings. Then I want to create an array for my results. Then I want to loop
+through the strings and use getBookByTitle and getBookByAuthor to push books
+that match those strings. Then I want to create a new array that merges results
+and filters through and get rid of duplicates. Then return the merged results.
 */
 Library.prototype.multiSearch = function(entry) {
   userEntry = entry.split(" ");
@@ -196,9 +189,8 @@ Library.prototype.setLibrary = function () {
 
 /*
 getLibrary: I want to get and information that changed from the localStorage
-and make it an object again
-Then I want to loop through the bookShelf and make that new object a book so
-it can be updated
+and make it an object again. Then I want to loop through the bookShelf and
+make that new object a book so it can be updated.
 */
 Library.prototype.getLibrary = function () {
   this.bookShelf = JSON.parse(localStorage.getItem("storedStuff"));
