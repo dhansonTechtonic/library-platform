626194c13f798b01c7ed311c1e8ea0cbcc22f070
Delaney Hanson <delaney.hanson@techtonic.com>
2018-10-05 09:21:25 -0600

    adds more test cases

--- js/book.js
+++ js/book.js
@@ -27,7 +27,7 @@ gLibrary.addBook(new Book("Lord of the Flies", "William Golding", 224, 1954))
 
 gLibrary.addBooks([new Book("Anthem","Ayn Rand",66,1938), new Book("Welcome to Night Vale","Joseph Fink & Jeffrey Cranor",416,2015), new Book("Carry On","Rainbow Rowell",528,2015), new Book("Fangirl","Rainbow Rowell",448,2013), new Book("Eleanor & Park","Rainbow Rowell",336,2013), new Book("Born A Crime","Trevor Noah",304,2016), new Book("Animal Farm","Georgoe Orwell",112,1945), new Book("The Giver","Lois Lowry",240,1993), new Book("The Wind Singer","William Nicholson",354,2000), new Book("Slaves of the Mastery","William Nicholson",352,2001), new Book("Firesong","William Nicholson",352,2002), new Book("Odd Thomas","Dean Koontz",446,2003), new Book("Atlas Shrugged","Ayn Rand",1168,1957), new Book("Glass and Gardens: Solarpunk Summers","Julia K. Patt & Wendy Nikel",292,2018), new Book("The Screwtape Letters","C. S. Lewis",160,1942), new Book("The Rise and Fall of Darth Vader","Ryder Windham",197,2007), new Book("The Fellowship of the Ring","J. R. R. Tolkien",423,1954), new Book("The Two Towers","J. R. R. Tolkien",352,1954), new Book("The Return of the King","J. R. R. Tolkien",416,1955), new Book("The Hitchhiker's Guide to the Galaxy","Douglas Adams",224,1979)])
 
-gLibrary.removeBookByTitle("Lord of the Flies")
+gLibrary.removeBookByTitle("Animal Farm")
 
 gLibrary.removeBookByAuthor("J. R. R. Tolkien")
 
@@ -46,4 +46,6 @@ gLibrary.getAuthors()
 gLibrary.getRandomAuthorName()
 
 gLibrary.theBigSearch("the Adams William")
+
+gLibrary.bookShelf[0].editBook("Lady of the Flies")
 */
